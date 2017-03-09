bf0cb0bd5ef2889adc75e882237314016074d531
Maxi <Maxi@Maxi-PC>
2017-03-09 18:32:11 +0100

    fixed music

--- src/ex0009/Music.java
+++ src/ex0009/Music.java
@@ -2,6 +2,7 @@
 package ex0009;
 
 import java.io.File;
+import java.net.URL;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javax.sound.sampled.AudioInputStream;
@@ -13,12 +14,11 @@ public class Music implements Runnable
 {
 private void playSound()
 {
+  URL mariachiUrl = getClass().getResource("../music/Mariachi.wav");
   try
   {
     AudioInputStream audioInputStream =
-       AudioSystem.getAudioInputStream(
-         new File(
-           "F:/JumpRun/src/music/Mariachi.wav").getAbsoluteFile());
+       AudioSystem.getAudioInputStream(mariachiUrl);
 
     Clip clip = AudioSystem.getClip();
     clip.open(audioInputStream);
